2eb25e9bbfb6ce32d6b9d9c574e54640a037d022
Jesmarie <jesmarie.hernandez@upr.edu>
2017-12-14 18:43:40 -0400

    Request Validation and Alert

--- src/containers/Request.js
+++ src/containers/Request.js
@@ -4,7 +4,7 @@ import DatePicker from 'react-bootstrap-date-picker';
 import TimePicker from 'react-bootstrap-time-picker';
 import {Link} from "react-router-dom";
 import {Modal} from 'react-bootstrap'
-
+import AlertContainer from 'react-alert';
 
 import {
     FormGroup, FormControl, ControlLabel, ButtonToolbar, Button,
@@ -17,9 +17,16 @@ class Request extends Component {
 
     constructor(props, context) {
         super(props, context);
+
         let value = new Date().toISOString();
         let valueDate = false;
+        let valueStartTime = false;
+        let valueEndTime = false;
+        let valueFacility = false;
+        let valueOrganization = false;
+/*
         let valueForm = false;
+*/
 
         this.state = {
             organizations: [],
@@ -33,17 +40,20 @@ class Request extends Component {
             selectedFacilities: {},
             statusOptions: ['pending', 'approved', 'denied'],
             selectedStatus: {},
-            showModal: '',
 
+            showModal: '',
             titleValue: '',
             descriptionValue: '',
             guestValue: '',
             attendanceValue: '',
-            facilityValue: '',
-
+            facilityPicked: valueFacility,
             datePicked: valueDate,
+            startTimePicked: valueStartTime,
+            endTimePicked: valueEndTime,
+            organizationPicked: valueOrganization,
+/*
             formValidated: valueForm
-
+*/
         }
 
         this.onOrganizationSelected = this.onOrganizationSelected.bind(this);
@@ -55,7 +65,10 @@ class Request extends Component {
         this.onSubmit = this.onSubmit.bind(this);
         this.open = this.open.bind(this);
         this.close = this.close.bind(this);
-        this.handleChange = this.handleChange.bind(this);
+        this.handleChangeTitle = this.handleChangeTitle.bind(this);
+        this.handleChangeDescription = this.handleChangeDescription.bind(this);
+        this.handleChangeGuest = this.handleChangeGuest.bind(this);
+        this.handleChangeAttendance = this.handleChangeAttendance.bind(this);
 /*
         this.isFormValidated = this.isFormValidated.bind(this);
 */
@@ -178,6 +191,7 @@ class Request extends Component {
     }
 
     onOrganizationSelected(event) {
+        this.setState({ organizationPicked: true });
         event.preventDefault();
         console.log('Change happened');
         console.log(event.target.value);
@@ -192,6 +206,7 @@ class Request extends Component {
     }
 
     onFacilitiesSelected(event) {
+        this.setState({ facilityPicked: true });
         event.preventDefault();
         console.log('Change happened');
         console.log(event.target.value);
@@ -214,6 +229,7 @@ class Request extends Component {
     }
 
     onStartTimeSelected(event) {
+        this.setState({startTimePicked: true});
         console.log("Event: " + event);
 
         var date = new Date(null);
@@ -234,6 +250,8 @@ class Request extends Component {
     }
 
     onEndTimeSelected(event) {
+        this.setState({endTimePicked: true});
+
         var date = new Date(null);
         date.setSeconds(event); // specify value for SECONDS here
         var res = date.getTimezoneOffset() / 60; //Gives me the hours to offset the time
@@ -263,24 +281,58 @@ class Request extends Component {
 
     open() {
         console.log("Opeeeen");
-        if (this.state.datePicked === true && this.state.titleValue.length > 0)
+        console.log(isNaN(this.state.guestValue));
+        console.log(typeof(this.state.attendanceValue));
+        console.log(this.state.attendanceValue > 0);
+
+        if (this.state.titleValue.length > 0 &&
+            this.state.descriptionValue.length > 0 &&
+            (this.state.guestValue.length > 0 && isNaN(this.state.guestValue)) &&
+            (this.state.attendanceValue > 1) &&
+            this.state.datePicked === true &&
+            this.state.startTimePicked === true &&
+            this.state.endTimePicked === true)
         {
             this.setState({showModal: true});
         }
+
+        else
+        {
+
+            {this.showErrorAlert()}
+
+        }
     }
 
-/*    isFormValidated() {
-        console.log("Form validated entered");
-        console.log(this.activity.requestTitle);
-        if (this.state.datePicked === true && this.state.titleValue.length > 0)
-            {
-                console.log("Form validated entered 2");
-                this.setState({formValidated: true});
-            }
-    }*/
+    showErrorAlert = () => {
+        this.msg.error('Error in fields. Something is not right.', {time: 5000, type: 'error'});
 
-    handleChange(e) {
+    }
+
+        /*    isFormValidated() {
+                console.log("Form validated entered");
+                console.log(this.activity.requestTitle);
+                if (this.state.datePicked === true && this.state.titleValue.length > 0)
+                    {
+                        console.log("Form validated entered 2");
+                        this.setState({formValidated: true});
+                    }
+            }*/
+
+    handleChangeTitle(e) {
         this.setState({titleValue: e.target.value})
+    }
+
+    handleChangeDescription(e) {
+        this.setState({descriptionValue: e.target.value})
+    }
+
+    handleChangeGuest(e) {
+        this.setState({guestValue: e.target.value})
+    }
+
+    handleChangeAttendance(e) {
+        this.setState({attendanceValue: e.target.value})
     }
 
     render() {
@@ -328,7 +380,6 @@ class Request extends Component {
 
                     <Row>
                         <Col md={12}>
-                            {/*<Form horizontal onSubmit={this.onSubmit} name="activityRequest">*/}
                             <Form horizontal name="activityRequest">
                                 <br/>
 
@@ -336,58 +387,58 @@ class Request extends Component {
                                     <FormGroup id="needs-validation">
                                         <Col sm={4}>
                                             <Col componentClass={ControlLabel} for="validationCustom01">Full Name</Col>
-                                            <FormControl name="requesterName" id="validationCustom01" required/>
+                                            <FormControl name="requesterName" />
                                         </Col>
 
                                         <Col sm={4}>
                                             <Col componentClass={ControlLabel}>Identification Number</Col>
-                                            <FormControl name="studentIdentificationNumber" required/>
+                                            <FormControl name="studentIdentificationNumber" />
                                         </Col>
 
                                         <Col sm={3}>
                                             <Col componentClass={ControlLabel}>Role</Col>
-                                            <FormControl name="studentRole" required/>
+                                            <FormControl name="studentRole" />
                                         </Col>
                                     </FormGroup>
 
                                     <FormGroup>
                                         <Col sm={6}>
                                             <Col componentClass={ControlLabel}>Address 1</Col>
-                                            <FormControl name="studentAddress1" required/>
+                                            <FormControl name="studentAddress1" />
                                         </Col>
 
                                         <Col sm={6}>
                                             <Col componentClass={ControlLabel}>Address 2</Col>
-                                            <FormControl name="studentAddress2" required/>
+                                            <FormControl name="studentAddress2" />
                                         </Col>
                                     </FormGroup>
 
                                     <FormGroup>
                                         <Col sm={3}>
                                             <Col componentClass={ControlLabel}>City</Col>
-                                            <FormControl name="studentAddressCity" required/>
+                                            <FormControl name="studentAddressCity" />
                                         </Col>
 
                                         <Col sm={3}>
                                             <Col componentClass={ControlLabel}>State</Col>
-                                            <FormControl name="studentAddressState" required/>
+                                            <FormControl name="studentAddressState"/>
                                         </Col>
 
                                         <Col sm={3}>
                                             <Col componentClass={ControlLabel}>Country</Col>
-                                            <FormControl name="studentAddressCountry" required/>
+                                            <FormControl name="studentAddressCountry" />
                                         </Col>
 
                                         <Col sm={3}>
                                             <Col componentClass={ControlLabel}>Zip Code</Col>
-                                            <FormControl name="studentAddressZipCode" required/>
+                                            <FormControl name="studentAddressZipCode" />
                                         </Col>
                                     </FormGroup>
 
                                     <FormGroup>
                                         <Col sm={4}>
                                             <Col componentClass={ControlLabel}>Telephone</Col>
-                                            <FormControl name="studentTelephone" required/>
+                                            <FormControl name="studentTelephone" />
                                         </Col>
                                     </FormGroup>
                                 </Panel>
@@ -398,25 +449,25 @@ class Request extends Component {
                                     <FormGroup>
                                         <Col sm={3}>
                                             <Col componentClass={ControlLabel}>Title</Col>
-                                            <FormControl name="requestTitle" value={this.state.titleValue} placeholder="Ex. Venta de Mantecados" type="text" onChange={this.handleChange}/>
+                                            <FormControl name="requestTitle" value={this.state.titleValue} placeholder="Ex. Venta de Mantecados" type="text" onChange={this.handleChangeTitle} />
                                         </Col>
 
                                         <Col sm={9}>
                                             <Col componentClass={ControlLabel}>Description</Col>
-                                            <FormControl name="activityDescription" required/>
+                                            <FormControl name="activityDescription" value={this.state.activityDescription} placeholder="Ex. Recaudacion de fondos" type="text" onChange={this.handleChangeDescription} />
                                         </Col>
 
                                     </FormGroup>
 
                                     <FormGroup>
                                         <Col sm={3}>
-                                            <Col componentClass={ControlLabel}>Invited Guests</Col>
-                                            <FormControl name="activityGuest" required/>
+                                            <Col componentClass={ControlLabel}>Guests</Col>
+                                            <FormControl name="activityGuest" value={this.state.activityDescription} placeholder="Ex. None" type="text" onChange={this.handleChangeGuest} />
                                         </Col>
 
                                         <Col sm={3}>
                                             <Col componentClass={ControlLabel}>Attendants</Col>
-                                            <FormControl name="activityAssistant" required/>
+                                            <FormControl name="activityAssistant" value={this.state.activityAssistant} type="number" min="1" start="1" onChange={this.handleChangeAttendance} />
                                         </Col>
 
                                         <Col md={3}>
@@ -425,7 +476,7 @@ class Request extends Component {
                                             <FormControl componentClass="select" name="selectFacilities"
                                                          onChange={this.onFacilitiesSelected}
 
-                                                         placeholder="select" required>
+                                                         placeholder="select">
                                                 <option>select</option>
                                                 {facilitiesOptions}
                                             </FormControl>
@@ -546,12 +597,10 @@ class Request extends Component {
 
                                     <Col md={6}>
 
-{/*
-                                        { this.state.formValidated === true ?
-*/}
-                                            <Button bsStyle="primary" type="button" onClick={this.open}>Submit</Button>
-{/*                                            : <Button bsStyle="primary" type="submit" onClick={this.isFormValidated}>Submit</Button> }*/}
+                                        <AlertContainer ref={a => this.msg = a} />
 
+
+                                        <Button bsStyle="primary" type="button" onClick={this.open}>Submit</Button>
                                         <Modal show={this.state.showModal} onHide={this.close}>
                                                 <Modal.Header closeButton>
                                                     <Modal.Title>Submit request?</Modal.Title>
